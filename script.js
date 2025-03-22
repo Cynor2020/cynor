ed956cd87d5ecaf3ff10eb11af61ff4298a10925
Sarthak Gadakh <145315653+Cynor2020@users.noreply.github.com>
2025-03-22 20:16:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - 50,
+                behavior: "smooth"
+            });
+        }
+    });
+});
+
+function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('active');
+    document.querySelector('.menu-toggle').classList.toggle('active');
+}
